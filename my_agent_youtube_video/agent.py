b0c30abad70e65c758b108a253ac53dcc335da32
MehmetHilmiEmel <mehmethilmi81@gmail.com>
2026-07-02 13:28:25 +0300

    basic single agent aplication with google-adk

---
+++ my_agent_youtube_video/agent.py
@@ -0,0 +1,19 @@
+from google.adk.agents.llm_agent import Agent
+
+def get_current_time(city: str) -> dict:
+    """Returns the current time in a specified city."""
+    return {"status": "success", "city": city, "time": "10:30 AM"}
+
+
+
+root_agent = Agent(
+    model='gemini-3.5-flash',
+    name='youtube_agent',
+    description='A helpful assistant for user questions.',
+    instruction='Answer user questions to the best of your knowledge. Her bir cevaptan sonra da kullanıcıya soru sor.',
+    tools=[get_current_time]
+)
+
+
+
+
